cd672adbaaa5663100e9204eea6854169e2bf1bd
thanhnv1493 <131841581+thanhnv1493@users.noreply.github.com>
2023-05-23 15:17:10 +0700

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -11,32 +11,22 @@ gen64() {
 	}
 	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
-
-# Thay đổi token và chat_id dưới đây bằng thông tin của bot Telegram và chat nhận tin nhắn
-TOKEN="5839097114:AAHwufhmUCxgnKFAg_B9HJKWZJx3a9GhLVI"
-CHAT_ID="654576348"
-
-send_message_to_telegram() {
-	MESSAGE="$1"
-	curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d "chat_id=$CHAT_ID" -d "text=$MESSAGE" >/dev/null
-}
-
 install_3proxy() {
-	echo "installing 3proxy"
-	URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-	wget -qO- $URL | bsdtar -xvf-
-	cd 3proxy-3proxy-0.8.6
-	make -f Makefile.Linux
-	mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-	cp src/3proxy /usr/local/etc/3proxy/bin/
-	cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-	chmod +x /etc/init.d/3proxy
-	chkconfig 3proxy on
-	cd $WORKDIR
+    echo "installing 3proxy"
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-3proxy-0.8.6
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy/bin/
+    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
+    cd $WORKDIR
 }
 
 gen_3proxy() {
-	cat <<EOF
+    cat <<EOF
 daemon
 maxconn 1000
 nscache 65536
@@ -56,37 +46,38 @@ EOF
 }
 
 gen_proxy_file_for_user() {
-	cat >proxy.txt <<EOF
+    cat >proxy.txt <<EOF
 $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
 upload_proxy() {
-	local PASS=$(random)
-	zip --password $PASS proxy.zip proxy.txt
-	URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+    local PASS=$(random)
+    zip --password $PASS proxy.zip proxy.txt
+    URL=$(curl -s --upload-file proxy.zip https://keep.sh/proxy.zip)
 
-	send_message_to_telegram "Proxy is ready! Format IP:PORT:LOGIN:PASS\nDownload zip archive from: $URL\nPassword: $PASS"
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download zip archive from: ${URL}"
+    echo "Password: ${PASS}"
+
 }
-
 gen_data() {
-	seq $FIRST_PORT $LAST_PORT | while read port; do
-		echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
-	done
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+    done
 }
 
 gen_iptables() {
-	cat <<EOF
-	$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+    cat <<EOF
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
 }
 
 gen_ifconfig() {
-	cat <<EOF
+    cat <<EOF
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
-
 echo "installing apps"
 yum -y install gcc net-tools bsdtar zip >/dev/null
 

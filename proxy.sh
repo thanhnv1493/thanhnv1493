83ef7fc7afd579d887bc0719a7b6ff1774f6f9b3
thanhnv1493 <131841581+thanhnv1493@users.noreply.github.com>
2023-05-23 13:28:08 +0700

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -11,22 +11,32 @@ gen64() {
 	}
 	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
+
+# Thay đổi token và chat_id dưới đây bằng thông tin của bot Telegram và chat nhận tin nhắn
+TOKEN="5839097114:AAHwufhmUCxgnKFAg_B9HJKWZJx3a9GhLVI"
+CHAT_ID="@proxythanh45_bot"
+
+send_message_to_telegram() {
+	MESSAGE="$1"
+	curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d "chat_id=$CHAT_ID" -d "text=$MESSAGE" >/dev/null
+}
+
 install_3proxy() {
-    echo "installing 3proxy"
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    chkconfig 3proxy on
-    cd $WORKDIR
+	echo "installing 3proxy"
+	URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+	wget -qO- $URL | bsdtar -xvf-
+	cd 3proxy-3proxy-0.8.6
+	make -f Makefile.Linux
+	mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+	cp src/3proxy /usr/local/etc/3proxy/bin/
+	cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+	chmod +x /etc/init.d/3proxy
+	chkconfig 3proxy on
+	cd $WORKDIR
 }
 
 gen_3proxy() {
-    cat <<EOF
+	cat <<EOF
 daemon
 maxconn 1000
 nscache 65536
@@ -40,41 +50,39 @@ users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
 
 gen_proxy_file_for_user() {
-    cat >proxy.txt <<EOF
+	cat >proxy.txt <<EOF
 $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
 upload_proxy() {
-    local PASS=$(random)
-    zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+	local PASS=$(random)
+	zip --password $PASS proxy.zip proxy.txt
+	URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
 
-    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-    echo "Download zip archive from: ${URL}"
-    echo "Password: ${PASS}"
-
+	send_message_to_telegram "Proxy is ready! Format IP:PORT:LOGIN:PASS\nDownload zip archive from: $URL\nPassword: $PASS"
 }
+
 gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
-    done
+	seq $FIRST_PORT $LAST_PORT | while read port; do
+		echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+	done
 }
 
 gen_iptables() {
-    cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+	cat <<EOF
+	$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
 }
 
 gen_ifconfig() {
-    cat <<EOF
+	cat <<EOF
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
@@ -87,4 +95,35 @@ install_3proxy
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd
+mkdir $WORKDIR && cd $_
+
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+
+echo "How many proxy do you want to create? Example 500"
+read COUNT
+
+FIRST_PORT=10000
+LAST_PORT=$(($FIRST_PORT + $COUNT))
+
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
+
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/rc.local <<EOF
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
+ulimit -n 10048
+service 3proxy start
+EOF
+
+bash /etc/rc.local
+
+gen_proxy_file_for_user
+
+upload_proxy
